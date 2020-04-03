d974288c050a324e3f29e8c3872c1c119625c49d
rw3iss <rw3iss@gmail.com>
2020-04-03 13:40:38 -0400

    latest

--- tmux.sh
+++ tmux.sh
@@ -3,8 +3,8 @@ tmux new-session -d
 tmux split-window -v
 tmux select-pane -t 0
 
-tmux send-keys -t 0 'cd /home/rweiss/mud && tt++ main.tin' Enter
-tmux send-keys -t 1 'cd /home/rweiss/mud && tail -fs .5 logs/*.log' Enter
+tmux send-keys -t 0 'cd /home/rw3iss/mud && tt++ main.tin' Enter
+tmux send-keys -t 1 'cd /home/rw3iss/mud && tail -fs .5 logs/*.log' Enter
 
 tmux resize-pane -U 14
 
