09d4a309264824394e1d0772c1da9f3771b1e7f0
rw3iss <rw3iss@gmail.com>
2020-04-04 16:38:14 -0400

    Bot fixed, and player whitelisting

--- tmux.sh
+++ tmux.sh
@@ -3,8 +3,8 @@ tmux new-session -d
 tmux split-window -h
 tmux select-pane -t 0
 
-tmux send-keys -t 0 'cd /mud2 && tt++ main.tin' Enter
-tmux send-keys -t 1 'cd /mud2 && tail -fs .5 logs/*.log' Enter
+tmux send-keys -t 0 'cd /mud && tt++ main.tin' Enter
+tmux send-keys -t 1 'cd /mud && tail -fs .5 logs/*.log' Enter
 
 tmux resize-pane -R 10
 
