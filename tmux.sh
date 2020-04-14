6344e15b7e9d324b3de0b6fa3f5a120401e6186b
rw3iss <rw3iss@gmail.com>
2020-04-14 12:57:24 -0400

    Some mip updates etc

--- tmux.sh
+++ tmux.sh
@@ -6,6 +6,6 @@ tmux select-pane -t 0
 tmux send-keys -t 0 'cd /mud && tt++ main.tin' Enter
 tmux send-keys -t 1 'cd /mud && tail -fs .5 logs/*.log' Enter
 
-tmux resize-pane -U 14
+tmux resize-pane -R 20
 
 tmux -2 attach-session -d 
